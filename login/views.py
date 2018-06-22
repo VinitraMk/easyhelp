b4131ce14ba68d095796d61b1ce65a394bee8603
VinitraMk <vinitramk@gmail.com>
2018-06-22 17:50:31 +0530

    version 1

---
+++ login/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.views.generic import View
+from .models import User as UserModel
+from .forms import UserForm
+# Create your views here.
+
+class Users(View):
+    template_name='login/users.html'
+
+    def get(self,request):
+        user_list=[]
+        form_user=UserForm()
+        users=UserModel.objects.all()[:50]
+
+        for user in users:
+            user_list.append({'email':user.email,'password':user.password})
+
+        return render(request,self.template_name, {
+            'title':'Users List',
+            'user_list':user_list,
+            'form_user':form_user
+            }
+            )
+
+    def post(self,request):
+        form_user=UserForm(request.POST)
+        if form_user.is_valid():
+            form_user.save()
+            return HttpResponseRedirect('/login/')
+
+
